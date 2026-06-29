2642380de472d5418b0270e867a8ee2ef29fcabb
Om Prakash <om02092002@gmail.com>
2026-06-29 19:38:05 +0530

    add new codes

--- funcation.py
+++ funcation.py
@@ -9,11 +9,66 @@
 #-----------------------------------------------------------
 #  average of 3 numbers:
 
-def calc_avg(a, b, c):
-    sum = a + b + c
-    avg =  sum / 3
-    print(avg)
-    return avg
+# def calc_avg(a, b, c):
+#     sum = a + b + c
+#     avg =  sum / 3
+#     print(avg)
+#     return avg
 
-calc_avg(98, 97 ,95)
-#--------------------------------------------------------------
+# calc_avg(98, 97 ,95)
+#-------------------------------------------------------------- 
+
+#WAF to print the length of a list.(list is the parameter)
+
+# cities = ["delhi", "pune","noi_da","gur_g_aon", "mumbai","chennai"]
+# heroes = ["thor","iron man","captain america", "shakti man"]
+
+# def print_len(list):
+#     print(len(list))
+
+# print_len(cities)
+# print_len(heroes)
+
+#------------------------------------------------------------------------
+
+#wap to print the element of a list in a single line.(list ia the parameter)
+
+# cities = ["delhi", "pune","noi_da","gur_g_aon", "mumbai","chennai"]
+# heroes = ["thor","iron_man","captain_america", "shakti_man"]
+
+# def print_len(list):
+#     print(len(list))
+
+# print_len(cities)    
+
+# def print_list(list):
+#     for item in list:
+#         print(item, end = " ")
+
+# print_list(heroes)            
+   
+#-----------------------------------------------------------------------------------
+
+#WAP to find the factorial of n. (n is the parameters.)
+
+# def cal_fact(n):
+#     fact = 1
+#     for i in range(1, n+1):
+#         fact *= i
+#     print(fact)
+
+# cal_fact(5)     #120
+
+#_______________________________________________________________________________
+#WAP to convert USD to INR.
+
+# def convert(usd_val):
+#     inr_val = usd_val * 83
+#     print(usd_val, "USD =", inr_val, "INR")
+
+# convert(45)    
+        
+
+#_________________________________________________________________________________
+
+
