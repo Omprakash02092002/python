493b8d833b8a1c116009739bbcc3416237ffe074
Om Prakash <om02092002@gmail.com>
2026-06-29 18:11:15 +0530

    udate new code

--- funcation.py
+++ funcation.py
@@ -1,7 +1,19 @@
 #function definition
 
-def calc_sum(a, b):  # a, b is parameters
-    return(a + b)
+# def calc_sum(a, b):  # a, b is parameters
+#     return(a + b)
 
-sum = calc_sum(1,2)   # function call ; 1,2 is arguments
-print(sum)
+# sum = calc_sum(1,2)   # function call ; 1,2 is arguments
+# print(sum)
+
+#-----------------------------------------------------------
+#  average of 3 numbers:
+
+def calc_avg(a, b, c):
+    sum = a + b + c
+    avg =  sum / 3
+    print(avg)
+    return avg
+
+calc_avg(98, 97 ,95)
+#--------------------------------------------------------------
